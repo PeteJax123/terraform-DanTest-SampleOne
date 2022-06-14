201d4ab4af3fb34ef6ec64aff0088d0aa96b6699
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2022-06-14 17:02:13 +0530

    Update new_provider.tf

--- new_provider.tf
+++ new_provider.tf
@@ -1,10 +1,13 @@
-{
-  "data": {
-    "type": "DanTest-registry-providers",
-    "attributes": {
-      "name": "aws",
-      "namespace": "hashicorp",
-      "registry-name": "private"
-    }
+module "myresource_tagging" {
+  source  = "claranet/tagging/azurerm"
+  version = "x.x.x"
+
+  nb_resources = 2
+  resource_ids = [var.myresource_id_1, var.myresource_id_2]
+  behavior     = "merge" # Must be "merge" or "overwrite"
+
+  tags = {
+    "foo"        = "bar"
+    "monitoring" = "true"
   }
 }
