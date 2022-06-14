4e16bc4cc73ce79c4b2fffb2918218206c81303c
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2022-06-14 16:25:20 +0530

    Create new_provider.tf

---
+++ new_provider.tf
@@ -0,0 +1,10 @@
+{
+  "data": {
+    "type": "DanTest-registry-providers",
+    "attributes": {
+      "name": "aws",
+      "namespace": "hashicorp",
+      "registry-name": "private"
+    }
+  }
+}
